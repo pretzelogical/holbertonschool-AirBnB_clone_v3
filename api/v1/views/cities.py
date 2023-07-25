38a2994e2172c9deb6897c5a17ee5ec29e9264a9
PaintRock <6126@holbertonstudents.com>
2023-07-25 03:34:08 +0000

    cities v2

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -4,14 +4,18 @@ from api.v1.views import app_views
 from flask import jsonify, request, abort
 from models import storage
 from models.city import Cities
+from models.state import States
 
 
-@app_views.route('/cities', methods=['GET'],
-                 strict_slashes=False)
-def get_cities():
-    """ Gets the list of all City objects """
-    cities = storage.all(City).values()
-    return jsonify([city.to_dict() for city in cities])
+@app.route('/states/<string:state_id>/cities',
+           method=['GET'], strict_slashes=False)
+def get_cities_by_state(state_id):
+    """ Retrives City objects of a State """
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    cities = [city.to_dict() for city in state.cities]
+    return jsonify(cities)
 
 
 @app_views.route('/cities/<string:city_id>', methods=['GET'],
@@ -27,7 +31,7 @@ def get_city(city_id):
 @app_views.route('/cities/<string:city_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_city(city_id):
-    """ why"""
+    """ Delete a City object"""
     city = storage.get(City, city_id)
     if not city:
         abort(404)
@@ -36,14 +40,19 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/cities', methods=['POST'],
+@app_views.route('/states/<string:state_id>/cities', methods=['POST'],
                  strict_slashes=False)
-def create_post():
-    if request.is_json is False:
-        abort(400, "Not a JSON")
+def create__city_post():
+    """ Creates a city """
+    state = storage.get(Stae, state_id)
+    if not state:
+        abort(404)
     data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
     if 'name' not in data:
         abort(400, "Missing name")
+    data['state_id'] = state_id
     city = City(**data)
     city.save()
     return jsonify(city.to_dict()), 201
@@ -52,17 +61,17 @@ def create_post():
 @app_views.route('/cities/<string:city_id>', methods=['PUT'],
                  strict_slashes=False)
 def update_city(city_id):
-    if request.is_json is False:
-        abort(400, 'Not a JSON')
-    data = request.get_json()
-    cities = storage.all(City)
-    s_key = "City." + city_id
-    if s_key not in cities:
+    """ Updates a City object """
+    city = storage.get(City, city_id)
+    if not city:
         abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
     # Ignore keys: id, created_at, and updated_at
-    ignored_keys = ['id', 'created_at', 'updated_at', 'city_id']
+    ignored_keys = ['id', 'created_at', 'updated_at', 'state_id']
     for key, value in data.items():
         if key not in ignored_keys:
-            setattr(cities[s_key], key, value)
-    storage.save()
-    return jsonify(cities[s_key].to_dict()), 200
+            setattr(city, key, value)
+    city.save()
+    return jsonify(cities.to_dict()), 200
